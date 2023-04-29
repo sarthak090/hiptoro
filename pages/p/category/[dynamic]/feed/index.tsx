fa53f00f28a8690793543eab668b53a5a286ae75
unknown <sarthakkaushik090@gmail.com>
2023-04-29 12:12:03 +0530

    Type Error Fixed

--- pages/p/category/[dynamic]/feed/index.tsx
+++ pages/p/category/[dynamic]/feed/index.tsx
@@ -18,11 +18,15 @@ export const getServerSideProps: GetServerSideProps = async (
   const dynamic = ctx.params !== undefined ? ctx.params.dynamic : "404";
 
   const c = await fetch(
-    `https://secureback.hiptoro.com/p/category/$${dynamic}/feed/`
+    `https://secureback.hiptoro.com/p/category/${dynamic}/feed/`
   ).then((r) => r.text());
 
   const format = c.replaceAll(`secureback.hiptoro.com`, "www.hiptoro.com");
   res.setHeader("Content-Type", "text/xml");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=20, stale-while-revalidate=59"
+  );
   res.write(format);
   res.end();
   return {
