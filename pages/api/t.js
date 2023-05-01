afd435f09f31d672665351bdf7dd17bdf5e42db7
unknown <sarthakkaushik090@gmail.com>
2023-05-01 13:47:02 +0530

    Custom RSS Feed :

--- pages/api/t.js
+++
@@ -1,8 +0,0 @@
-import { testRss } from ".././../utils/generateRssFeed";
-export default async function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/xml");
-  const xmlTest = await testRss();
-
-  res.end(xmlTest);
-}
