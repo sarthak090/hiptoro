c91f6bef9737acb683b25d53a12f003ceed88352
unknown <sarthakkaushik090@gmail.com>
2023-05-01 10:19:33 +0530

    Scroll Improved

--- pages/p/[slug].tsx
+++ pages/p/[slug].tsx
@@ -20,13 +20,11 @@ export default function SinglePost(props: any) {
       const res = await fetch(url);
       const newPosts = await res.json();
       if (newPosts.length > 0) {
-        const excludedPosts = newPosts
-          .filter((p: any) => p.id !== post.id)
-          .slice(0, posts.length + 1);
+        const excludedPosts = newPosts.filter((p: any) => p.id !== post.id);
+
         setPosts((post: any) => [...post, ...excludedPosts]);
-        if (posts.length === 7) {
-          setHasMore(false);
-        }
+
+        setHasMore(false);
       } else {
         setHasMore(false);
       }
@@ -35,11 +33,13 @@ export default function SinglePost(props: any) {
     return (
       <div className="container mx-auto max-w-site-full">
         <InfiniteScroll
-          dataLength={8}
+          dataLength={4}
           next={getMorePost}
           hasMore={hasMore}
           loader={<Loading />}
-          endMessage={<h4>Nothing more to show</h4>}
+          endMessage={
+            <h4 className="text-center my-8 ">Nothing more to show</h4>
+          }
         >
           {posts.map((p) => (
             <div key={p.id}>
