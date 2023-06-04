c4af81da92cd3646c831d82aed93e08a49201f4e
Tahir Yasin <tahiryasin123@gmail.com>
2023-06-04 16:43:53 +0400

    debugging

--- middleware.js
+++ middleware.js
@@ -2,9 +2,9 @@
 export default function middleware(req, res) {
     let pid = req.url.split("=").pop();
     
-    if (pid > 0) {
-        return Response.redirect(new URL(`https://secureback.hiptoro.com/?page_id=${pid}`))
-    }
+    //if (pid > 0) {
+        return Response.redirect(new URL(`https://secureback.hiptoro.com/`))
+    //}
 }
 // config with custom matcher
 export const config = {
