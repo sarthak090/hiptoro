7cbe1bda06652043d75d5b5fb349aeffc8b58f21
Tahir Yasin <tahiryasin123@gmail.com>
2023-06-04 17:25:00 +0400

    redirection fixed

--- middleware.js
+++ middleware.js
@@ -1,12 +1,13 @@
 // middleware.js with Vercel CLI
 export default function middleware(req, res) {
-    let pid = req.url.split("=").pop();
-    
-    //if (pid > 0) {
-        return Response.redirect(new URL(`https://secureback.hiptoro.com/`))
-    //}
+    let pid = req.url.split("/").pop();
+    if(pid>0){
+		return Response.redirect(new URL(`https://secureback.hiptoro.com/?page_id=${pid}`));
+	}
 }
 // config with custom matcher
 export const config = {
-    matcher: '/',
+    matcher: '/:path*',
 };
+
+
