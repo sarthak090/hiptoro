a0ef4f8890c0072956841251a92b89fef4a114fb
unknown <sarthakkaushik090@gmail.com>
2023-06-07 01:00:59 +0530

    Added LazyLoad

--- components/Ads/AutoAds.tsx
+++ components/Ads/AutoAds.tsx
@@ -1,29 +1,66 @@
 import Script from "next/script";
 
+import LazyLoad from "react-lazyload";
+
 export default function AutoAds() {
   return (
     <>
-      <br />
-      <br />
-      <Script
-        async={true}
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7099984888351146"
-        crossOrigin="anonymous"
-        onError={(e) => {
-          console.error("Script failed to load", e);
-        }}
-        strategy="lazyOnload"
-      />
-      {/* <!-- Adsense New --> */}
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-7099984888351146"
-        data-ad-slot="5392495815"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <Script strategy="lazyOnload">{`(adsbygoogle = window.adsbygoogle || []).push({});`}</Script>
+      <LazyLoad offset={100} once>
+        <div>
+          <script
+            defer
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7099984888351146"
+            crossOrigin="anonymous"
+            onError={(e) => {
+              console.error("Script failed to load", e);
+            }}
+          />
+          {/* <!-- Adsense New --> */}
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-7099984888351146"
+            data-ad-slot="5392495815"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>{` 
+       setTimeout(() => {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      
+      }, 0);
+   
+      `}</script>
+        </div>
+      </LazyLoad>
     </>
   );
 }
+
+// export default function AutoAds() {
+//   return (
+//     <>
+//       <br />
+//       <br />
+//       <Script
+//         async={true}
+//         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7099984888351146"
+//         crossOrigin="anonymous"
+//         onError={(e) => {
+//           console.error("Script failed to load", e);
+//         }}
+//         strategy="lazyOnload"
+//       />
+//       {/* <!-- Adsense New --> */}
+//       <ins
+//         className="adsbygoogle"
+//         style={{ display: "block" }}
+//         data-ad-client="ca-pub-7099984888351146"
+//         data-ad-slot="5392495815"
+//         data-ad-format="auto"
+//         data-full-width-responsive="true"
+//       ></ins>
+//       <Script strategy="lazyOnload">{`(adsbygoogle = window.adsbygoogle || []).push({});`}</Script>
+//     </>
+//   );
+// }
