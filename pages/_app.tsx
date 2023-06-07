c442a9a6c76ae66e5d02efeee9544044ac70759d
unknown <sarthakkaushik090@gmail.com>
2023-06-07 14:51:38 +0530

    Connatix Strategy Changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,7 +89,7 @@ export default function App({ Component, pageProps }: AppProps) {
         referrerPolicy="no-referrer-when-downgrade"
         src="https://udmserve.net/udm/img.fetch?sid=15497;tid=1;dt=6;"
       />
-      <Script strategy="lazyOnload">
+      <Script strategy="worker">
         {`!function(n){if(!window.cnx){window.cnx={},window.cnx.cmd=[];var t=n.createElement('iframe');t.src='javascript:false'; t.display='none',t.onload=function(){var n=t.contentWindow.document,c=n.createElement('script');c.src='//cd.connatix.com/connatix.player.js?cid=d7375c7c-a8aa-4449-891e-4b3af534cf41',c.setAttribute('async','1'),c.setAttribute('type','text/javascript'),n.body.appendChild(c)},n.head.appendChild(t)}}(document);`}
       </Script>
       {/* <!-- Google tag (gtag.js) --> */}
