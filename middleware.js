26db8ee188a18a4a666e3d77cf5ec75fa59bd6a8
unknown <sarthakkaushik090@gmail.com>
2023-06-07 16:43:38 +0530

    middleware added

--- middleware.js
+++ middleware.js
@@ -1,11 +1,12 @@
 // middleware.js with Vercel CLI
 export default function middleware(req, res) {
-  if (process.env.NODE_ENV === "development") {
-    return;
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   return;
+  // }
 
   const requestUrl = req.url;
-  if (requestUrl.includes("page_id")) {
+
+  if (requestUrl.includes("page_id") || requestUrl.includes("?p")) {
     let pid = req.url.split("=").pop();
     return Response.redirect(
       new URL(`https://secureback.hiptoro.com/?page_id=${pid}`)
