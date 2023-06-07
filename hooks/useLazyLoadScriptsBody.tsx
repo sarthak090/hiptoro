7c8530c6309eb718babd270eb7cae4d6e31ece07
unknown <sarthakkaushik090@gmail.com>
2023-06-08 00:35:10 +0530

    Added Connatix Back

---
+++ hooks/useLazyLoadScriptsBody.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useLazyLoadScripts = (scripts: string[]) => {
+  const [scriptsLoaded, setScriptsLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!scriptsLoaded && window.pageYOffset > 0) {
+        // Load scripts when the page is scrolled or when there's some interaction
+        scripts.forEach((scriptSrc) => {
+          const scriptElement = document.createElement("script");
+          scriptElement.src = scriptSrc;
+          scriptElement.async = true;
+          document.body.appendChild(scriptElement);
+        });
+
+        setScriptsLoaded(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scripts, scriptsLoaded]);
+
+  return scriptsLoaded;
+};
+
+export default useLazyLoadScripts;
