7ff95bcb943bd7baf17b2736fe863d55dd98c075
unknown <sarthakkaushik090@gmail.com>
2023-07-01 18:18:50 +0530

    added img svif

--- utils/images/getWebpBase64.js
+++ utils/images/getWebpBase64.js
@@ -8,7 +8,8 @@ export async function getWebpBase64(imageSrc = "") {
   const response = await axios.get(imageSrc, { responseType: "arraybuffer" });
   const imageData = response.data;
   const optimizedImage = await sharp(imageData)
-    .webp({ quality: 80 })
+    .avif({ quality: 80 })
+
     .toBuffer();
   const base64Image = Buffer.from(optimizedImage).toString("base64");
   const dataURI = `data:image/webp;base64,${base64Image}`;
