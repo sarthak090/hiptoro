10322a98c2d64cb8ba5f60345faa1a5a89746a95
unknown <sarthakkaushik090@gmail.com>
2023-07-04 12:44:35 +0530

    trailing slashes removed from sitemap

--- next.config.js
+++ next.config.js
@@ -26,6 +26,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -82,7 +82,8 @@ export const getServerSideProps: GetServerSideProps = async ({
     const format = resp
       .replace(`//secureback.hiptoro.com/news-sitemap.xsl`, "/v.xsl")
       .replace("//secureback.hiptoro.com/main-sitemap.xsl", "/main-sitemap.xsl")
-      .replaceAll(`secureback.hiptoro.com`, "www.hiptoro.com");
+      .replaceAll(`secureback.hiptoro.com`, "www.hiptoro.com")
+      .replace(/(https?:\/\/\S+?)\/(?=\s|<)/g, "$1");
 
     res.setHeader("Content-Type", "text/xml");
 
